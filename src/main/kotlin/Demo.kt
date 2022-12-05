ec30bb0d29acba2a73f34c43bcb2de4568eae54b
falling <zhaowenrui_8866@163.com>
2022-12-05 18:21:31 +0800

    Changes

--- src/main/kotlin/Demo.kt
+++ src/main/kotlin/Demo.kt
@@ -1,13 +1,12 @@
 fun main() {
-    demo("hello") {
-        println(it)
-    }
+    val data1 = Demo1(1)
+    val data2 = Demo1(1)
+    val demo1 = Demo()
+    val demo2 = Demo()
+    println(data1 == data2)
+    println(demo1 == demo2)
 }
 
-fun demo(str: String, func: (String) -> Unit) {
-    func(str)
-}
+data class Demo1(var num: Int)
 
-fun show(str: String) {
-    println(str)
-}
+class Demo
