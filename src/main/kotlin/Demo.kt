424359115c42d3fa409e9de6637e11f32b4e0076
falling <zhaowenrui_8866@163.com>
2022-12-07 19:29:55 +0800

    Changes

--- src/main/kotlin/Demo.kt
+++ src/main/kotlin/Demo.kt
@@ -1,20 +1,6 @@
 fun main() {
-    val context = Context().apply5 {
-        toast("success")
-        toast(it)
-        toast(name)
-    }
-    println(context.info)
-}
-
-class Context {
-    val info = "info"
-    val name = "demo"
-
-    fun toast(str: String) = println("toast: $str")
-}
-
-inline fun Context.apply5(lambda: Context.(String) -> Unit): Context {
-    lambda(info)
-    return this
+    val a = listOf(1, 2, 3, 4)
+    val b = listOf("a", "b", "c")
+    val c = a.zip(b)
+    println(c)
 }
