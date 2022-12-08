1b2f7e933b1b308681531aeed524b61eae413021
falling <zhaowenrui_8866@163.com>
2022-12-08 09:48:41 +0800

    Changes

--- src/main/kotlin/Demo.kt
+++ src/main/kotlin/Demo.kt
@@ -1,6 +1,19 @@
 fun main() {
-    val a = listOf(1, 2, 3, 4)
-    val b = listOf("a", "b", "c")
-    val c = a.zip(b)
-    println(c)
+    val a = create {
+        123
+    }.map {
+        "[$this]"
+    }.map {
+        "@@$this@@"
+    }.observer {
+        println(this)
+    }
 }
+
+inline fun <output> create(action: () -> output) = Demo(action())
+
+class Demo<T>(val valueItem: T)
+
+inline fun <I, O> Demo<I>.map(mapAction: I.() -> O) = Demo(mapAction(valueItem))
+
+inline fun <I> Demo<I>.observer(observerAction: I.() -> Unit) = observerAction(valueItem)
